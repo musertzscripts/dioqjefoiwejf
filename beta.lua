adadffd9bc64be3204f8b2f0dc2275bab988d755
yes <shtymane@gmail.com>
2025-03-30 15:27:08 +0100

    Update beta.lua

--- beta.lua
+++ beta.lua
@@ -5,7 +5,7 @@ local RunService = game:GetService("RunService")
 local player = Players.LocalPlayer
 local mouse = player:GetMouse()
 
-local clicking = false -- Controls the click loop
+local clicking = false -- Controls the clicking loop
 
 -- Function to start clicking rapidly
 local function startClicking()
@@ -21,18 +21,33 @@ local function stopClicking()
     clicking = false
 end
 
--- Constantly check if the mouse is over another player
-RunService.RenderStepped:Connect(function()
-    local target = mouse.Target
+-- Function to check if the target is another player
+local function isHoveringOverPlayer(target)
     if target and target.Parent then
         local targetPlayer = Players:GetPlayerFromCharacter(target.Parent)
-        if targetPlayer and targetPlayer ~= player then
-            if not clicking then
-                startClicking()
-            end
-        else
-            stopClicking()
+        return targetPlayer and targetPlayer ~= player
+    end
+    return false
+end
+
+-- PC: Detects if the mouse is over another player
+RunService.RenderStepped:Connect(function()
+    if isHoveringOverPlayer(mouse.Target) then
+        if not clicking then
+            startClicking()
         end
+    else
+        stopClicking()
+    end
+end)
+
+-- Mobile: Detects if the player taps on another player
+UserInputService.TouchTapInWorld:Connect(function(position, processed)
+    if processed then return end -- Ignore UI touches
+
+    local ray = workspace:Raycast(position, Vector3.new(0, -1, 0))
+    if ray and isHoveringOverPlayer(ray.Instance) then
+        startClicking()
     else
         stopClicking()
     end
